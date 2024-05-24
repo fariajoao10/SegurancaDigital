13c198c282005206f4c401b636fe58fc8f0d157c
fariajoao10 <161661842+fariajoao10@users.noreply.github.com>
2024-05-23 22:11:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,52 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
+let tamanhoSenha = 5;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
+const campoSenha = document.querySelector('#campo-senha');
+
+const checkbox = document.querySelectorAll('.checkbox');
+
+console.log(checkbox[0].checked);
+
+
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+
+
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+            let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+            numeroAleatorio = Math.floor(numeroAleatorio);
+            senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    }
+
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){ 
-    tamanhoSenha = tamanhoSenha - 1;
-}
-
-console.log(botoes);
-function diminuiTamanho(){ 
-    tamanhoSenha = tamanhoSenha - 1;
-    numeroSenha.textContent = tamanhoSenha
+function diminuiTamanho(){
+    if (tamanhoSenha > 1) {
+        //tamanhoSenha = tamanhoSenha - 1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho() {
-    tamanhoSenha = tamanhoSenha + 1;
+    if (tamanhoSenha < 20) {
+        //tamanhoSenha = tamanhoSenha + 1;
+        tamanhoSenha++;
+}
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 
 
